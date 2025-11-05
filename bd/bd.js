9e6e53f5d94e57027f96039c93c50898d1e01912
jacquelinemcti24-alt <jaqcuelinemc.ti24@utsjr.edu.mx>
2025-11-04 22:28:26 -0600

    Ajuste de codigo

--- bd/bd.js
+++ bd/bd.js
@@ -5,7 +5,7 @@ import 'dotenv/config'
 export async function conectarBD(){//Conectamos nuestra base de datos de mongo
 	try{
 		console.log("-----AKJHDKHFKJAHFKJ----")
-		const conexion = await mongoose.connect(process.env.SECRET_MONGO)
+		const conexion = await mongoose.connect(process.env.SECRET_MONGO);
 		//CONEXION LOCAL const conexion = mongoose.connect("mongodb://localhost:27017/(nombre de la aplicacion)")
 		console.log("conexion establecida con mongo atlas")
 	}
--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.use("/",rutas)
 app.use(()=>{})
 
 app.use((req,res,next)=>{
-    res.status(404).reder("404")
+    res.status(404).render("404")
 })
 
-const PORT= process.env.PORT ||3000
+const PORT= process.env.PORT || 3000
 
 app.listen(PORT, function(){
     console.log("SERVIDOR EN http://localhost:"+PORT)
